c69b8f0fb692b13a9b5337d9bfc36e509c748f44
Rcaer <rcastilhoavelino@gmail.com>
2018-04-22 23:43:49 +0100

    UI Basic features
    Testing
    Header - dropdown menus (messages, notification and tasks)
    
    Sidebar - sidebar menu, menu items
    
    Body - tab Items and tab box

--- RShinyAppDashboard/ui.R
+++ RShinyAppDashboard/ui.R
@@ -2,12 +2,68 @@ library(shiny)
 library(shinydashboard)
 
 # Define UI for application
-header <- dashboardHeader(
-  dropdownMenu(type = "messages")
-)
-sidebar <- dashboardSidebar()
-body <- dashboardBody()
 
+# Header
+header <- dashboardHeader(
+  dropdownMenu(
+    type = "messages",
+    messageItem(
+      from = "Lucy",
+      message = "You can view the International Space Station!",
+      href = "https://spotthestation.nasa.gov/sightings/"
+    ),
+    # Add a second messageItem() 
+    messageItem(
+      from = "Lucy",
+      message = "Learn more about the International Space Station",
+      href = "https://spotthestation.nasa.gov/faq.cfm"
+    )
+  ),
+  dropdownMenu(
+    type = "notifications",
+    notificationItem(
+      text = "The International Space Station is overhead!")
+  ),
+  dropdownMenu(
+    type = "tasks",
+    taskItem(
+      text = "Mission Learn Shiny Dashboard",
+      value = 10)
+  )
+)
+
+# Sidebar
+sidebar <- dashboardSidebar(
+  sidebarMenu(
+    # Create two `menuItem()`s, "Dashboard" and "Inputs"
+    menuItem(
+      "Dashboard",
+      tabName = "dashboard"
+    ),
+    menuItem(
+      "Inputs",
+      tabName = "inputs"
+    )
+  )
+)
+
+#Body
+body <- dashboardBody(
+  # Create a tabBox
+  tabItems(
+    tabItem(
+      tabName = "dashboard",
+      tabBox(
+        title = "International Space Station Fun Facts",
+        tabPanel("Fun Fact 1"),
+        tabPanel("Fun Fact 2")
+      )
+    ),
+    tabItem(tabName = "inputs")
+  )
+)
+
+# UI
 shinyUI(
   dashboardPage(header, sidebar, body)
 )
